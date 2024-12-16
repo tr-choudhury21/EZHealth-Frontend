6cc7e3d1671eb44057494acb8fdd05983c11fd60
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-12-16 14:50:13 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,60 @@
+import React , {useContext, useEffect} from "react";
+import { Context } from "./main";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import HomePage from "./pages/Home"
+import Navbar from "./components/Navbar";
+import LoginPage from "./pages/Login";
+import RegistrationPage from "./pages/Register";
+import AboutUsPage from "./pages/About";
+import Footer from "./components/Footer";
+import DoctorsPage from "./pages/Doctors";
+import ServicesPage from "./components/Service";
+import AppointmentPage from "./pages/Appointment";
+
+function App() {
+
+  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+  
+
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUsPage/>} />
+          <Route path="/doctors" element={<DoctorsPage/>} />
+          <Route path="/service" element={<ServicesPage/>} />
+          <Route path="/appointment" element={<AppointmentPage/>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+        </Routes>
+        <Footer/>
+        <ToastContainer position="top-center"/>
+      </Router>
+    </>
+  )
+}
+
+export default App
